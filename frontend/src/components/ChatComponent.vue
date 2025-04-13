1bb540de8654257b0228a9e14d27758a0319b180
Alex Karpov <karpov.aleksey.84@gmail.com>
2025-04-13 04:07:52 -0400

    Add chat skeleton

---
+++ frontend/src/components/ChatComponent.vue
@@ -0,0 +1,77 @@
+<template>
+  <vue-advanced-chat
+      :current-user-id="currentUserId"
+      :rooms="JSON.stringify(rooms)"
+      :messages="JSON.stringify(messages)"
+      :room-actions="JSON.stringify(roomActions)"
+      :rooms-loaded="true"
+      :messages-loaded="this.messagesLoaded"
+      @add-room="onAddRoom"
+  />
+</template>
+
+<script>
+import { register } from 'vue-advanced-chat'
+register()
+
+export default {
+  data() {
+    return {
+      currentUserId: '1234',
+      rooms: [],
+      messages: [],
+      roomActions: [
+        { name: 'inviteUser', title: 'Invite User' },
+        { name: 'removeUser', title: 'Remove User' },
+        { name: 'deleteRoom', title: 'Delete Room' }
+      ],
+      messagesLoaded: true
+    }
+  },
+  methods: {
+    onAddRoom() {
+      this.messages = [];
+      const roomId = crypto.randomUUID();
+      const room = {
+        roomId: roomId,
+        roomName: roomId,
+        users: [
+          {
+            _id: 'user1',
+            username: 'user1'
+          }
+        ],
+        messages: [],
+      }
+      // Keep only one room
+      this.rooms = [room]
+      this.fetchMessages({room});
+    },
+    fetchMessages({ room }) {
+      console.log('loading messages in room: ' + room.roomId)
+      this.messagesLoaded = false
+
+      // bot message
+      setTimeout(() => {
+        this.messages.push({
+          _id: Math.random().toString(36).substr(2, 9),
+          content: 'Hello! How can I help you today?',
+          senderId: this.currentUserId,
+          timestamp: new Date().toISOString()
+        });
+        this.messagesLoaded = true
+      }, 1000)
+
+      // user message
+      setTimeout(() => {
+        this.messages.push({
+          _id: Math.random().toString(36).substr(2, 9),
+          content: 'Hi! I have a question about your product.',
+          senderId: 'user1',
+          timestamp: new Date().toISOString()
+        });
+      }, 2000)
+    }
+  }
+}
+</script>
